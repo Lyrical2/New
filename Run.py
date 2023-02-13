45d2f5c1fb7e7ddf73e28bfbc7579f3ba4153f4f
Lyrical2 <121253899+Lyrical2@users.noreply.github.com>
2023-02-13 21:38:57 +0300

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,12 +1,12 @@
 import platform
 import os
-try:os.system("xdg-open https://www.facebook.com/jafar.sadiq.1675275?mibextid=ZbWKwL")
-except:pass	
+#try:os.system("xdg-open https://www.facebook.com/jafar.sadiq.1675275?mibextid=ZbWKwL")
+#except:pass	
 try:os.system("clear")
 except:pass	
 os.system('termux-setup-storage')
 os.system('git pull')
-try:os.system('touch /sdcard/james.txt')
+#try:os.system('touch /sdcard/james.txt')
 except:pass
 try:os.system('touch .proxy.txt')
 except:pass
